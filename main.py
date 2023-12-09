a11e94df26f9d8b978957b7502692205e47abe29
niba2828 <k.futaba2828@gmail.com>
2023-12-09 15:41:00 +0900

    Add: /menu/:menu_idのMockを実装

--- main.py
+++ main.py
@@ -21,8 +21,18 @@ def get_menu_list():
             "specials": [],
             "alc_percent": 34.0,
             "ingredients": [
-                {"id": 1, "name": "ウィスキー", "unit": "ml", "amount_ml": 45,},
-                {"id": 2, "name": "スイートベルモット", "unit": "ml", "amount_ml": 15,},
+                {
+                    "id": 1,
+                    "name": "ウィスキー",
+                    "unit": "ml",
+                    "amount_ml": 45,
+                },
+                {
+                    "id": 2,
+                    "name": "スイートベルモット",
+                    "unit": "ml",
+                    "amount_ml": 15,
+                },
                 {
                     "id": 3,
                     "name": "アロマティックビダーズ",
@@ -32,6 +42,38 @@ def get_menu_list():
         }
     ]
 
+
+@app.get("/menu/{menu_id}", response_model=MenuModel)
+def get_menu_by_id(menu_id):
+    return {
+        "id": 1,
+        "name": "マンハッタン",
+        "description": "マンハッタン（英: Manhattan）は、ウイスキーベースのカクテルの一種である。カクテルの女王と呼ばれる",
+        "image_url": "https://liqul.com/upimg/2020/06/015-manhattan01.jpg",
+        "method": "stir",
+        "style": "short",
+        "specials": [],
+        "alc_percent": 34.0,
+        "ingredients": [
+            {
+                "id": 1,
+                "name": "ウィスキー",
+                "unit": "ml",
+                "amount_ml": 45,
+            },
+            {
+                "id": 2,
+                "name": "スイートベルモット",
+                "unit": "ml",
+                "amount_ml": 15,
+            },
+            {
+                "id": 3,
+                "name": "アロマティックビダーズ",
+                "unit": "dash",
+            },
+        ],
+    }
 
 
 def main() -> None:
