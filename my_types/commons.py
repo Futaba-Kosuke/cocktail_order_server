2488d7ecbefa399b306cbec5ae2c6c14e4190561
niba2828 <k.futaba2828@gmail.com>
2023-12-10 15:13:16 +0900

    Update: 単位にdropを追加

--- my_types/commons.py
+++ my_types/commons.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-UnitType = Literal["ml", "tea_spoon", "dash", "slice"]
+UnitType = Literal["ml", "tea_spoon", "dash", "drop", "slice"]
 MethodType = Literal["stir", "shake", "build"]
 StyleType = Literal["short", "long"]
 OrderStatusType = Literal["processing", "calling", "already"]
